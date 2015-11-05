6c429a9c68fee6a34432f39b1d5433991fb80c4f
CHilke1 <chad.hilke@gmail.com>
2015-11-05 09:08:32 -0600

    updated program

--- Fibonacci2.py
+++ Fibonacci2.py
@@ -3,19 +3,23 @@ class Fibonacci(object):
 		self.start = start
 		self.max = max
 		
-	def fibonacci(self, num):
-		if num == 0 or num == 1:
-			return num
+	def fibonacci(self, max):
+		if max == 0 or max == 1:
+			return max
 		else:
-			return self.fibonacci(num - 1) + self.fibonacci(num - 2)
+			return self.fibonacci(max - 1) + self.fibonacci(max - 2)
 			
 	def printfib(self, start, max):
 		for i in range(start, max):
 			print("Fibonacci of %d %d: " % (i, self.fibonacci(i)))
 			
-start = int(input("enter start: "))
-max = int(input("Enter maximum: "))
-myfib = Fibonacci(start, max)
-myfib.printfib(start, max)
 
-#http://stackoverflow.com/questions/625083/python-init-and-self-what-do-they-do
+while True:
+	start = int(input("enter start : "))
+	max = int(input("Enter maximum: "))
+	myfib = Fibonacci(start, max)
+	myfib.printfib(start, max)
+	quit = input("Run again?Y/N: ")
+	if quit == "N" or quit == "n":
+		break
+
